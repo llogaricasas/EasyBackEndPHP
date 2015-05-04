29443c10040d6ddf6d7a670179522af96bcd7398
Llogari Casas <llogaricasas@kaleidoscop.net>
2015-05-04 11:54:20 +0200

    Create classStringGenerator.php

---
+++ class/classStringGenerator.php
@@ -0,0 +1,18 @@
+<?php
+class StringGenerator{
+	
+	private $string = NULL;
+	
+	public function __construct($length){
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++){
+        	$randomString.= $characters[rand(0, strlen($characters) - 1)];
+		}
+		$this->string = $randomString;
+	}
+	public function getRandomString(){
+    	return $this->string;	
+	}
+}
+?>
